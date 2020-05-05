ba62a0715a0799fd7c66e9d2d9965205a40428fd
mevanmaele <mvanmaele@talusanalytics.com>
2020-05-05 18:26:54 -0400

    styling

--- src/components/misc/Util.js
+++ src/components/misc/Util.js
@@ -8,3 +8,13 @@ export const isEmpty = d => {
   if (Object.keys(d).length === 0) return true;
   else return false;
 };
+
+// const getApiUrl = () => {
+//   if (process.env.NODE_ENV === "production") {
+//     if (window.location.href.search("https") > -1)
+//       return "https://gida-tracking-api.ghscosting.org/";
+//     else return "http://gida-tracking-api-dev.us-west-1.elasticbeanstalk.com/";
+//   } else return "http://localhost:5002";
+// };
+//
+// export const API_URL = getApiUrl();
--- src/components/views/data/Data.js
+++ src/components/views/data/Data.js
@@ -190,34 +190,25 @@ const Data = () => {
   else
     return (
       <div className={styles.data}>
-        <h1>
-          Welcome to COVID
-          <br />
-          Policy Tracker
-        </h1>
-        <div className={styles.columnText}>
-          <p>
-            The COVID Policy Tracker provides access to a comprehensive list of
-            policies implemented at all levels of government globally to address
-            the COVID-19 pandemic. In many cases, subnational governments have
-            led the COVID-19 response. For simple search, each policy has been
-            categorized into the type of measure taken, in addition to
-            implementation date and authorizing agency. In addition, policies
-            can be identified by legal authority, when available, for
-            implementing each policy listed. Where available, PDFs or links to
-            the policies are included.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
-            nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
-            volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
-            ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
-            consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate
-            velit esse molestie consequat, vel illum dolore eu feugiat nulla
-            facilisis at vero eros et accumsan et iusto odio dignissim qui
-            blandit praesent luptatum zzril delenit augue duis dolore te feugait
-            nulla facilisi.
-          </p>
+        <div className={styles.header}>
+          <h1>
+            Welcome to COVID
+            <br />
+            Policy Tracker
+          </h1>
+          <div className={styles.columnText}>
+            <p>
+              The COVID Policy Tracker provides access to a comprehensive list
+              of policies implemented at all levels of government globally to
+              address the COVID-19 pandemic. In many cases, subnational
+              governments have led the COVID-19 response. For simple search,
+              each policy has been categorized into the type of measure taken,
+              in addition to implementation date and authorizing agency. In
+              addition, policies can be identified by legal authority, when
+              available, for implementing each policy listed. Where available,
+              PDFs or links to the policies are included.
+            </p>
+          </div>
         </div>
         <Drawer
           {...{
