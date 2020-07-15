cf9836fa024ca502e8320e651a45dc4067b2ffc5
Ryan <rzimmerman@talusanalytics.com>
2020-07-15 16:22:46 -0600

    Changing contact % tick values, and formatting

--- src/components/views/PolicyModel/PolicyPlot/PolicyPlot.js
+++ src/components/views/PolicyModel/PolicyPlot/PolicyPlot.js
@@ -493,7 +493,8 @@ const PolicyModel = props => {
         {/* /> */}
         <VictoryAxis
           dependentAxis
-          tickFormat={tick => (tick === parseInt(tick) ? parseInt(tick) : null)}
+          tickValues={[0, 50, 100]}
+          tickFormat={tick => tick + "%"}
           offsetX={50}
           crossAxis={false}
           label={
