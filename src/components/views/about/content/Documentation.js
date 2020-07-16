0356e31ee74848424405d807452d9ab854144645
mevanmaele <mvanmaele@talusanalytics.com>
2020-07-16 09:43:29 -0400

    reorg documentation components

---
+++ src/components/views/about/content/Documentation.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import classNames from "classnames";
+import styles from "./documentation.module.scss";
+
+// assets
+
+const Documentation = ({ ...props }) => {
+  return <div>Placeholder</div>;
+};
+
+export default Documentation;
