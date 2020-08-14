43a203599b1ce5ced12278c2c1e524db4abceb3a
mevanmaele <mvanmaele@talusanalytics.com>
2020-08-14 09:49:36 -0400

    List only info@talus email address in accessibility note
    We reduced the number of emails listed from 3 to 1 to improve user accessibility and reduce potential for confusion about the most appropriate account to email with accessibility inquiries.

--- src/components/common/AccessibilityNote/AccessibilityNote.js
+++ src/components/common/AccessibilityNote/AccessibilityNote.js
@@ -3,23 +3,38 @@ import classNames from "classnames";
 import styles from "./accessibilitynote.module.scss";
 
 const AccessibilityNote = () => {
+  // CONSTANTS
+  // list of emails to display
+  const emails = (
+    <a href="mailto:info@talusanalytics.com" target="_blank">
+      info@talusanalytics.com
+    </a>
+  );
+
+  // original list of three emails as of Aug 14, 2020
+  // const emails = (
+  //   <>
+  //     <a href="mailto:outbreaks@georgetown.edu" target="_blank">
+  //       outbreaks@georgetown.edu
+  //     </a>
+  //     ,{" "}
+  //     <a href="mailto:covidlocal@nti.org" target="_blank">
+  //       covidlocal@nti.org
+  //     </a>
+  //     , or{" "}
+  //     <a href="mailto:info@talusanalytics.com" target="_blank">
+  //       info@talusanalytics.com
+  //     </a>
+  //   </>
+  // );
+
+  // JSX
   return (
     <p className={classNames(styles.accessibilityNote, styles.warning)}>
       <div className={styles.warningIcon} />
       <div>
         If you have any accessibility issues using this site, please contact us
-        directly at{" "}
-        <a href="mailto:outbreaks@georgetown.edu" target="_blank">
-          outbreaks@georgetown.edu
-        </a>
-        ,{" "}
-        <a href="mailto:covidlocal@nti.org" target="_blank">
-          covidlocal@nti.org
-        </a>
-        , or{" "}
-        <a href="mailto:info@talusanalytics.com" target="_blank">
-          info@talusanalytics.com
-        </a>
+        directly at {emails}.
       </div>
     </p>
   );
