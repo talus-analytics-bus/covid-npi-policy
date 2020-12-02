4b8e9a30e187acf3b529e03b979f4fecb74a3f5c
Ryan <rzimmerman@talusanalytics.com>
2020-12-02 11:34:21 -0700

    Adding header, map, and styling to policy list page

--- src/components/views/PolicyPage/ListPoliciesPage/ListPoliciesPage.js
+++ src/components/views/PolicyPage/ListPoliciesPage/ListPoliciesPage.js
@@ -8,6 +8,11 @@ import {
 } from "../PolicyRouter/PolicyLoaders";
 
 import ExpandingSection from "./ExpandingSection/ExpandingSection";
+import PolicySummary from "./PolicySummary/PolicySummary";
+
+import * as MiniMap from "../MiniMap/MiniMap";
+
+import styles from "./ListPoliciesPage.module.scss";
 
 const ListPoliciesPage = props => {
   const { iso3, state } = useParams();
@@ -60,40 +65,103 @@ const ListPoliciesPage = props => {
     }
   };
 
+  console.log(props.policyObject);
+
   return (
-    <section>
-      {props.policyObject &&
-        Object.entries(props.policyObject).map(([categoryName, category]) => (
-          <ExpandingSection
-            key={categoryName}
-            onOpen={() => loadDescriptionsByCategory(categoryName)}
-          >
-            <h1>
-              {categoryName} {Object.keys(category).length}
-            </h1>
-            {Object.entries(category).map(([subcatName, subcat]) => (
+    <article>
+      <section className={styles.introSection}>
+        <div className={styles.text}>
+          <h1>{state !== "national" ? state : iso3} COVID-19 Policies</h1>
+          <div className={styles.quickFacts}>
+            <div className={styles.policies}>
+              {12345}
+              <br /> Policies
+            </div>
+            <div className={styles.status}>
+              New Normal
+              <br />
+              Policy Status
+            </div>
+            <div className={styles.status}>
+              {12345}
+              <br /> 7-Day Cases
+            </div>
+            <div className={styles.status}>
+              {12345}% higher than
+              <br /> previous week
+            </div>
+          </div>
+          <p>
+            {state ? state : iso3} has been in a {`New Normal`} policy status
+            for the past {`two months`}, based on analysis of {12345} measures
+            from {12345} {state ? "state and county" : "national"} policies
+            covering{" "}
+            {Object.keys(props.policyObject)
+              .map(pm => pm.toLowerCase())
+              .slice(0, -1)
+              .join(", ")}
+            , and{" "}
+            {Object.keys(props.policyObject)
+              .slice(-1)
+              .join("")
+              .toLowerCase()}
+            .
+          </p>
+        </div>
+        <div className={styles.miniMapHolder}>
+          <MiniMap.SVG
+            country={iso3}
+            state={state}
+            counties={["Unspecified"]}
+          />
+        </div>
+      </section>
+      <section>
+        {props.policyObject &&
+          Object.entries(props.policyObject).map(([categoryName, category]) => (
+            <div className={styles.topLevelContainer} key={categoryName}>
               <ExpandingSection
-                key={subcatName}
-                onOpen={() =>
-                  loadDescriptionsBySubCategory(categoryName, subcatName)
-                }
+                onOpen={() => loadDescriptionsByCategory(categoryName)}
               >
-                <h2>
-                  {subcatName} {Object.keys(subcat).length}
-                </h2>
-                {Object.entries(subcat).map(([policyID, policy]) => (
-                  <div key={policyID}>
-                    {/* <ExpandingSection key={policyID}> */}
-                    <h3>{policyID}</h3>
-                    <p>{policyID} policy details</p>
-                    {/* </ExpandingSection> */}
-                  </div>
-                ))}
+                <div className={styles.topLevelHeader}>
+                  <div className={styles.icon} />
+                  <h1>
+                    {categoryName}
+                    {/* {Object.keys(category).length} */}
+                  </h1>
+                </div>
+                <div className={styles.categoryContainer}>
+                  {Object.entries(category).map(([subcatName, subcat]) => (
+                    <ExpandingSection
+                      key={subcatName}
+                      onOpen={() =>
+                        loadDescriptionsBySubCategory(categoryName, subcatName)
+                      }
+                    >
+                      <div className={styles.secondLevelHeader}>
+                        <div className={styles.markerDot} />
+                        <h2>
+                          {subcatName}
+                          {/* {Object.keys(subcat).length} */}
+                        </h2>
+                      </div>
+                      <div className={styles.secondLevelContainer}>
+                        {Object.entries(subcat).map(([policyID, policy]) => (
+                          <PolicySummary
+                            key={policyID}
+                            id={policyID.replace("ID", "")}
+                            policy={policy}
+                          />
+                        ))}
+                      </div>
+                    </ExpandingSection>
+                  ))}
+                </div>
               </ExpandingSection>
-            ))}
-          </ExpandingSection>
-        ))}
-    </section>
+            </div>
+          ))}
+      </section>
+    </article>
   );
 };
 
