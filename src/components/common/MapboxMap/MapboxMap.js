627a04ca01843de4f8176162e52ca5c95f7f3996
mevanmaele <mvanmaele@talusanalytics.com>
2020-12-03 17:48:56 -0500

    Remove log

--- src/components/common/MapboxMap/MapboxMap.js
+++ src/components/common/MapboxMap/MapboxMap.js
@@ -521,8 +521,6 @@ const MapboxMap = ({
         // choose one feature from among the detected features to use as target.
         // circle takes precedence over fill feature since it is drawn on top
         const feature = circleFeature || fillFeature;
-        console.log("feature");
-        console.log(feature);
 
         // deselect the currently selected feature
         if (selectedFeature !== null) {
