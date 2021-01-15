52e0f21eca11009d17bef1ff093e4f526e5393c8
Ryan <rzimmerman@talusanalytics.com>
2021-01-15 09:51:08 -0700

    Fixing search results breadcrumbs

--- src/components/views/PolicyPage/PolicySummary/PolicySummary.js
+++ src/components/views/PolicyPage/PolicySummary/PolicySummary.js
@@ -7,11 +7,6 @@ import Tippy from "@tippyjs/react";
 import "tippy.js/dist/tippy.css";
 import "tippy.js/themes/light.css";
 
-import {
-  CATEGORY_FIELD_NAME,
-  SUBCATEGORY_FIELD_NAME,
-} from "../PolicyRouter/PolicyLoaders";
-
 const TITLE_WORD_LIMIT = 10;
 
 const formatDate = date => {
@@ -59,29 +54,39 @@ const PolicySummary = props => {
         state: { path: props.path },
       }}
     >
-      {props.breadcrumbs && (
-        // <p className={styles.breadcrumbs}>
-        //   {props.path &&
-        //     props.path
-        //       .filter(s => s !== "children")
-        //       .slice(0, -2)
-        //       .join(" / ")}
-        //   &nbsp; / {props.path && props.path.slice(-3)[0]}
-        // </p>
-        <div className={styles.metadata}>
-          <div>
-            <h1>Category</h1>
-            <h2>{props.policy[CATEGORY_FIELD_NAME]}</h2>
-          </div>
-          <div>
-            <h1>Subcategory</h1>
-            <h2>{props.policy[SUBCATEGORY_FIELD_NAME]}</h2>
-          </div>
-          <div className={styles.area}>
-            <h1>Affected Area</h1>
-            <h2>{props.policy.auth_entity[0].place.loc.split(",")[0]}</h2>
-          </div>
-        </div>
+      {props.showAllMetadata && (
+        <p className={styles.breadcrumbs}>
+          {props.path &&
+            props.path
+              .filter(
+                s =>
+                  ![
+                    "children",
+                    "Local",
+                    "Country",
+                    "State / Province",
+                  ].includes(s)
+              )
+              .slice(0, -2)
+              .map(e => (
+                <React.Fragment key={e}>{e} &nbsp; 〉 </React.Fragment>
+              ))}
+          {props.path && props.path.slice(-3)[0]}
+        </p>
+        // <div className={styles.metadata}>
+        //   <div>
+        //     <h1>Category</h1>
+        //     <h2>{props.policy[CATEGORY_FIELD_NAME]}</h2>
+        //   </div>
+        //   <div>
+        //     <h1>Subcategory</h1>
+        //     <h2>{props.policy[SUBCATEGORY_FIELD_NAME]}</h2>
+        //   </div>
+        //   <div className={styles.area}>
+        //     <h1>Affected Area</h1>
+        //     <h2>{props.policy.auth_entity[0].place.loc.split(",")[0]}</h2>
+        //   </div>
+        // </div>
       )}
       <div className={styles.metadata}>
         <div>
