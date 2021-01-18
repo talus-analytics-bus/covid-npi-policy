e992bfdc791585f05dff89f4c39426899dc62ef4
Ryan <rzimmerman@talusanalytics.com>
2021-01-18 16:23:31 -0700

    Truncating descriptions in plot popups to 50 words

--- src/components/views/PolicyPage/CaseloadPlotD3/PolicyBar/TooltipContent/TooltipContent.js
+++ src/components/views/PolicyPage/CaseloadPlotD3/PolicyBar/TooltipContent/TooltipContent.js
@@ -12,24 +12,36 @@ const formatDate = date => {
   });
 };
 
-const TooltipContent = props => (
-  <div className={styles.tooltipContent}>
-    <header>
-      <div>
-        <h1>Effective From</h1>
-        <h2> {formatDate(new Date(props.policy.date_start_effective))}</h2>
-      </div>
-      <div>
-        <h1>Ended</h1>
-        <h2>
-          {props.policy.date_end_actual === "active"
-            ? "Active"
-            : formatDate(new Date(props.policy.date_end_actual))}
-        </h2>
-      </div>
-    </header>
-    <p>{props.policy.desc}</p>
-  </div>
-);
+const TooltipContent = props => {
+  const descriptionWords = props.policy.desc.split(" ");
+
+  const wordLimit = 50;
+
+  const truncateDescription = descriptionWords.length > wordLimit;
+
+  const description = truncateDescription
+    ? descriptionWords.slice(0, wordLimit).join(" ") + "..."
+    : props.policy.desc;
+
+  return (
+    <div className={styles.tooltipContent}>
+      <header>
+        <div>
+          <h1>Effective From</h1>
+          <h2> {formatDate(new Date(props.policy.date_start_effective))}</h2>
+        </div>
+        <div>
+          <h1>Ended</h1>
+          <h2>
+            {props.policy.date_end_actual === "active"
+              ? "Active"
+              : formatDate(new Date(props.policy.date_end_actual))}
+          </h2>
+        </div>
+      </header>
+      <p>{description}</p>
+    </div>
+  );
+};
 
 export default TooltipContent;
