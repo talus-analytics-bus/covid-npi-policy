e4c1ea5d7be3129aaf051ffae53e4ed9452d8e91
Ryan <rzimmerman@talusanalytics.com>
2021-01-22 16:21:56 -0700

    Adding wrapper component that makes an expander section open and close on hover

---
+++ src/components/layout/nav/NavDropdown/NavDropDown.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+// This component needs to be moved
+import ExpandingSection from "../../../views/PolicyPage/ListPoliciesPage/PolicyList/ExpandingSection/ExpandingSection";
+
+const NavDropDown = props => {
+  let children = React.Children.toArray(props.children);
+  const [expanderOpen, setExpanderOpen] = React.useState();
+
+  return (
+    <div
+      onMouseEnter={() => setExpanderOpen(true)}
+      onMouseLeave={() => setExpanderOpen(false)}
+    >
+      <ExpandingSection floating clickAwayClose open={expanderOpen}>
+        <>{children[0]}</>
+        <>{children.slice(1)}</>
+      </ExpandingSection>
+    </div>
+  );
+};
+
+export default NavDropDown;
