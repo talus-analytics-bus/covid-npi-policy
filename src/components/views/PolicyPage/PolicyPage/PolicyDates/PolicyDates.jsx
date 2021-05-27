39e222bb411cef07cc3511b42d39e957d4a1fca8
Ryan <rzimmerman@talusanalytics.com>
2021-05-26 21:56:27 -0600

    component for the policy dates
    This might end up being the only place where the rows and columns system
    is used so I'm making a placeholder file for refactoring that.

---
+++ src/components/views/PolicyPage/PolicyPage/PolicyDates/PolicyDates.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+
+import courtChallengeIcon from "../../../../../assets/icons/CourtChallengeIcon.svg";
+
+import styles from "../PolicyPage2.module.scss";
+
+// TODO: replace with <FMT.Date> component
+const formatDate = date => {
+  if (!date) return undefined;
+  return date.toLocaleString("en-us", {
+    day: "numeric",
+    month: "short",
+    year: "numeric",
+    // timeZone: "UTC",
+  });
+};
+
+const PolicyDates = ({ policy }) => (
+  <>
+    <div className={styles.row}>
+      <div className={styles.col}>
+        <h3>Published in</h3>
+        <p>
+          <strong>{policy && policy.policy_name}</strong>
+        </p>
+      </div>
+    </div>
+
+    <div className={styles.headerInfoRow}>
+      <div className={styles.col}>
+        <h3>Effective from</h3>
+        <p>
+          <strong>
+            {policy && formatDate(new Date(policy.date_start_effective))}
+          </strong>
+        </p>
+      </div>
+      <div className={styles.col}>
+        <h3>Ended</h3>
+        <p>
+          <strong>
+            {policy &&
+              ((policy.date_end_actual &&
+                formatDate(new Date(policy.date_end_actual))) ||
+                "Active")}
+          </strong>
+        </p>
+      </div>
+      <div className={styles.col}>
+        <h3>Date Issued</h3>
+        <p>
+          <strong>
+            {policy &&
+              (policy.date_issued
+                ? formatDate(new Date(policy.date_issued))
+                : "Not Specified")}
+          </strong>
+        </p>
+      </div>
+      {policy && policy.court_challenges && (
+        <div className={styles.col}>
+          <div className={styles.courtChallengeIcon}>
+            <img src={courtChallengeIcon} alt="Court Challenge Icon" />
+            <h3>
+              Challenged <br />
+              In Court
+            </h3>
+          </div>
+        </div>
+      )}
+    </div>
+  </>
+);
+
+export default PolicyDates;
