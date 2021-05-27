2bd6478a9e39a4dd5f930c66a170325daf38f7d1
Ryan <rzimmerman@talusanalytics.com>
2021-05-26 21:56:52 -0600

    Refactoring the policy breadcrumbs out into a standalone display component

---
+++ src/components/views/PolicyPage/PolicyPage/PolicyBreadcrumbs/PolicyBreadcrumbs.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import PolicyCategoryIcon from "../../PolicyCategoryIcon/PolicyCategoryIcon";
+
+import styles from "./PolicyBreadcrumbs.module.scss";
+
+const PolicyBreadcrumbs = ({ iso3, state, policyObjectPath }) => (
+  <div className={styles.breadCrumbs}>
+    {iso3 !== "Unspecified" && (
+      <>
+        <PolicyCategoryIcon
+          category={policyObjectPath && policyObjectPath[0]}
+        />
+        <Link to={`/policies/${iso3}/${state}`}>
+          {policyObjectPath &&
+            policyObjectPath
+              .filter(
+                s =>
+                  ![
+                    "children",
+                    // "Local",
+                    // "Country",
+                    // "State / Province",
+                  ].includes(s)
+              )
+              .slice(0, -2)
+              .map(e => (
+                <React.Fragment key={e}>{e} &nbsp; 〉 </React.Fragment>
+              ))}
+          {policyObjectPath && policyObjectPath.slice(-3)[0]}
+        </Link>
+      </>
+    )}
+  </div>
+);
+
+export default PolicyBreadcrumbs;
