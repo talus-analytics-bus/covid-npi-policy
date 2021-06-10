a579179fa679091d2ae07ef26738beba0f51dc14
Ryan <rzimmerman@talusanalytics.com>
2021-06-10 11:52:51 -0600

    removing useEffect, since the popup is always visible while the slider is moving now

--- src/components/views/PolicyPage/ListPoliciesPage/PolicyEnvironmentPlot/Slider/Tooltip/Tooltip.jsx
+++ src/components/views/PolicyPage/ListPoliciesPage/PolicyEnvironmentPlot/Slider/Tooltip/Tooltip.jsx
@@ -8,41 +8,19 @@ const TooltipContent = ({
   sliderDate,
   highlightPolicies,
   highlightCaseload,
-}) => {
-  //   const [renderPolicies, setRenderPolicies] = useState();
-  //
-  //   console.log(highlightCaseload);
-  //
-  //   // make sure not to empty out the popup when highlightPolicies
-  //   // is undefined so that the closing animation is smooth
-  //   useEffect(() => {
-  //     if (highlightPolicies)
-  //       setRenderPolicies(
-  //         <>
-  //           <p>{highlightCaseload} new cases</p>
-  //           {Object.entries(highlightPolicies).map(([category, policies]) => (
-  //             <p key={category}>
-  //               {category}: ({policies.length})
-  //             </p>
-  //           ))}
-  //         </>
-  //       );
-  //   }, [highlightPolicies, highlightCaseload]);
-
-  return (
-    <div>
-      {/* <> */}
-      <p>{highlightCaseload} new cases</p>
-      {highlightPolicies &&
-        Object.entries(highlightPolicies).map(([category, policies]) => (
-          <p key={category}>
+}) => (
+  <div style={{ pointerEvents: "auto" }}>
+    <p>{highlightCaseload} new cases</p>
+    {highlightPolicies &&
+      Object.entries(highlightPolicies).map(([category, policies]) => (
+        <p key={category}>
+          <a href="/policymaps">
             {category}: ({policies.length})
-          </p>
-        ))}
-      {/* </> */}
-    </div>
-  );
-};
+          </a>
+        </p>
+      ))}
+  </div>
+);
 
 const Tooltip = ({
   handleYPos,
