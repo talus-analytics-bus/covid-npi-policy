03d83522d3d8a734d08dbf886447391e56f5c5cb
mvanmaele <mvanmaele@talusanalytics.com>
2021-12-02 11:48:49 -0500

    Start adding `DownloadOptions` comp (WIP, unused)

---
+++ src/components/views/data/DownloadOptions.tsx
@@ -0,0 +1,24 @@
+import { OptionCheckbox } from "components/common/OptionControls/OptionCheckboxSet/OptionCheckbox/OptionCheckbox";
+import React, { FC } from "react";
+
+type DownloadOptionsChoices = {
+  /**
+   * If true, Excel download includes all columns in dataset, otherwise it
+   * only includes the summary table info shown on the Data page. Defaults
+   * to false.
+   */
+  includeAllCols: boolean;
+};
+
+interface DownloadOptionsProps {
+  choices: DownloadOptionsChoices;
+}
+
+export const DownloadOptions: FC<DownloadOptionsProps> = ({ choices }) => {
+  return (
+    <OptionCheckbox
+      name={"Download all columns"}
+      value={choices.includeAllCols.toString()}
+    />
+  );
+};
