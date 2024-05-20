42b45beb896fcf11d15fa534adddb80f23613e7b
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2024-05-20 20:39:30 -0300

    Adding world and US map, with switching between them

--- src/components/views/PolicyCoverage/PolicyCoverage.tsx
+++ src/components/views/PolicyCoverage/PolicyCoverage.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useRef } from "react";
 import { useEffect } from "react";
 import styled from "styled-components";
 import { AmpPage } from "types";
@@ -9,6 +9,7 @@ import Map, {
   Layer,
   LngLat,
   MapLayerMouseEvent,
+  MapRef,
   NavigationControl,
   Source,
 } from "react-map-gl";
@@ -28,16 +29,46 @@ const useSetPageAndDisableLoading = (
 type SetPage = React.Dispatch<React.SetStateAction<AmpPage | null>>;
 type SetLoading = React.Dispatch<React.SetStateAction<boolean>>;
 
-interface PolicyCoverageProps {
-  setPage: SetPage;
-  setLoading: SetLoading;
-}
-
 const MapContainer = styled.div`
   position: relative;
   height: calc(100vh - 116px);
   top: 116px;
   width: 100%;
+`;
+
+const MapTypeSwitcher = styled.div`
+  position: absolute;
+  top: 30px;
+  right: 30px;
+  border-radius: 5px;
+  background-color: #ffffff;
+  box-shadow: inset 0px 0px 3px rgba(0, 0, 0, 0.1);
+  border: 1px solid #7b848e;
+  padding: 15px 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`;
+
+const MapSwitcherLabel = styled.label`
+  color: #1d1c1c;
+  font-family: "Open Sans";
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  display: flex;
+  align-items: flex-start;
+  gap: 8px;
+  margin: 0;
+
+  input {
+    position: relative;
+    top: 5px;
+    cursor: pointer;
+    padding: 0;
+    margin: 0;
+  }
 `;
 
 export interface PopupState {
@@ -46,25 +77,40 @@ export interface PopupState {
   setPopupState: React.Dispatch<React.SetStateAction<PopupState | null>>;
 }
 
+interface PolicyCoverageProps {
+  setPage: SetPage;
+  setLoading: SetLoading;
+}
+
+enum MapType {
+  USA = "USA",
+  World = "World",
+}
+
 const PolicyCoverage = ({ setPage, setLoading }: PolicyCoverageProps) => {
   useSetPageAndDisableLoading(setPage, setLoading);
 
+  const [mapType, setMapType] = React.useState(MapType.World);
   const [hoveredISO, setHoveredISO] = React.useState(" ");
   const [popupState, setPopupState] = React.useState<PopupState | null>(null);
 
-  const outlineLayer = {
-    id: `countries-outline`,
-    type: `line` as `line`,
-    source: `countries_v13c-6uk894`,
-    "source-layer": "countries_v13c-6uk894",
-    paint: {
-      "line-color": "blue",
-      "line-width": 2,
-    },
-    beforeId: "country-label",
-  };
+  const mapRef = useRef<MapRef | null>(null);
+
+  // const outlineLayer = {
+  //   id: `countries-outline`,
+  //   type: `line` as `line`,
+  //   source: `countries_v13c-6uk894`,
+  //   "source-layer": "countries_v13c-6uk894",
+  //   paint: {
+  //     "line-color": "blue",
+  //     "line-width": 2,
+  //   },
+  //   // beforeId: "country-label",
+  // };
 
   const onHover = useCallback((event: MapLayerMouseEvent) => {
+    console.log(event.features);
+    console.log(mapRef.current?.getStyle().layers);
     setHoveredISO(event.features?.[0]?.properties?.ISO_A3 ?? " ");
   }, []);
 
@@ -87,18 +133,37 @@ const PolicyCoverage = ({ setPage, setLoading }: PolicyCoverageProps) => {
     });
   }, []);
 
+  const handleChangeMapType = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.value === "USA") {
+      setMapType(MapType.USA);
+    } else {
+      setMapType(MapType.World);
+    }
+  };
+
+  let mapStyle = "mapbox://styles/nicoletalus/ckp5qwi392djb18qbvlf0hiku";
+  if (mapType === MapType.USA) {
+    mapStyle = "mapbox://styles/nicoletalus/ckq9vwu8t0w4w17k0nwj4z9kz";
+  }
+
   return (
     <MapContainer>
       <Map
+        ref={mapRef}
         mapboxAccessToken={MAPBOX_ACCESS_TOKEN!}
         // mapStyle="mapbox://styles/ryan-talus/clddahzv7007j01qbgn0bba8w"
-        mapStyle="mapbox://styles/nicoletalus/ckp5qwi392djb18qbvlf0hiku"
+        // mapStyle="mapbox://styles/nicoletalus/ckp5qwi392djb18qbvlf0hiku"
+        mapStyle={mapStyle}
         projection={{ name: "mercator" }}
         interactive={true}
         initialViewState={{
           longitude: 0,
           latitude: 15,
           zoom: 0,
+          bounds: [
+            [-150, -60],
+            [150, 70],
+          ],
           // these bounds are weird due to a bug in mapbox with non-mercator projections:
           // https://github.com/mapbox/mapbox-gl-js/issues/11284
           // bounds: [
@@ -116,23 +181,60 @@ const PolicyCoverage = ({ setPage, setLoading }: PolicyCoverageProps) => {
         <Source
           id="country-borders"
           type="vector"
-          url="mapbox://ryan-talus.2o1iyjoj"
-        >
-          {/* This layer paints all colors including grey background color */}
-          <Layer
-            key={outlineLayer.id}
-            {...outlineLayer}
-            filter={["==", "ISO_A3", hoveredISO]}
-          />
-          {
-            // <Layer key={countryLayer.id} {...countryLayer} />
-          }
-        </Source>
+          url="mapbox://nicoletalus.3a8qy0w1"
+        />
+
+        {
+          // <Source
+          //   id="country-borders"
+          //   type="vector"
+          //   url="mapbox://ryan-talus.2o1iyjoj"
+          // >
+        }
+        {/* This layer paints all colors including grey background color */}
+        {
+          // <Layer
+          //   key={outlineLayer.id}
+          //   {...outlineLayer}
+          //   filter={["==", "ISO_A3", hoveredISO]}
+          // />
+        }
+        {
+          // <Layer key={countryLayer.id} {...countryLayer} />
+        }
+        {
+          // </Source>
+        }
         {
           // <NavigationControl position="top-left" showCompass={false} />
           // {popupState && <MapPopup popupState={popupState} mapType={mapType} />}
         }
       </Map>
+      <MapTypeSwitcher>
+        <MapSwitcherLabel>
+          <input
+            type="radio"
+            name="mapType"
+            value={MapType.USA}
+            onChange={handleChangeMapType}
+          />
+
+          <span>
+            United States
+            <br />
+            (state-level-policies)
+          </span>
+        </MapSwitcherLabel>
+        <MapSwitcherLabel>
+          <input
+            type="radio"
+            name="mapType"
+            value={MapType.World}
+            onChange={handleChangeMapType}
+          />
+          World
+        </MapSwitcherLabel>
+      </MapTypeSwitcher>
     </MapContainer>
   );
 };
